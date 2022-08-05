7805f35728e73293b966c6a1c1bfc5aae56a5c1a
Ross Taylor-Turner <ross.turner001@googlemail.com>
2022-08-05 16:31:53 +0100

    Support for source and target colours in RequirementToColorMappingsTag

--- core/src/main/java/technology/rocketjump/saul/entities/tags/RequirementToColorMappingsTag.java
+++ core/src/main/java/technology/rocketjump/saul/entities/tags/RequirementToColorMappingsTag.java
@@ -11,7 +11,6 @@ import technology.rocketjump.saul.entities.model.physical.item.ItemType;
 import technology.rocketjump.saul.entities.model.physical.item.ItemTypeDictionary;
 import technology.rocketjump.saul.gamecontext.GameContext;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class RequirementToColorMappingsTag extends Tag {
@@ -23,7 +22,7 @@ public class RequirementToColorMappingsTag extends Tag {
 
 	@Override
 	public boolean isValid(TagProcessingUtils tagProcessingUtils) {
-		return args.size() % 2 == 0;
+		return args.size() % 3 == 0;
 	}
 
 	@Override
@@ -32,31 +31,28 @@ public class RequirementToColorMappingsTag extends Tag {
 	}
 
 	public void apply(Entity createdFurnitureEntity, Map<Long, Entity> itemsRemovedFromConstruction, ItemTypeDictionary itemTypeDictionary) {
-		Map<ItemType, ColoringLayer> configuration = new HashMap<>();
+		FurnitureEntityAttributes targetAttributes = (FurnitureEntityAttributes) createdFurnitureEntity.getPhysicalEntityComponent().getAttributes();
 
-		for (int configCursor = 0; configCursor < args.size(); configCursor += 2) {
+		for (int configCursor = 0; configCursor < args.size(); configCursor += 3) {
 			ItemType itemType = itemTypeDictionary.getByName(args.get(configCursor));
-			ColoringLayer coloringLayer =  ColoringLayer.valueOf(args.get(configCursor + 1));
+			ColoringLayer sourceColorLayer = ColoringLayer.valueOf(args.get(configCursor + 1));
+			ColoringLayer targetColorLayer = ColoringLayer.valueOf(args.get(configCursor + 2));
 
 			if (itemType == null) {
 				Logger.error("Unrecognised item type in " + this.getClass().getSimpleName() + " with name " + args.get(configCursor));
-			} else  if (!EnumUtils.isValidEnum(ColoringLayer.class, args.get(configCursor + 1))) {
-					Logger.error("Unrecognised " + ColoringLayer.class.getSimpleName() + " in " + this.getClass().getSimpleName() + " with name " + args.get(configCursor + 1));
+			} else if (!EnumUtils.isValidEnum(ColoringLayer.class, args.get(configCursor + 1)) ||
+					!EnumUtils.isValidEnum(ColoringLayer.class, args.get(configCursor + 2))) {
+				Logger.error("Unrecognised " + ColoringLayer.class.getSimpleName() + " in " + this.getClass().getSimpleName() + " with name " + args.get(configCursor + 1));
 			} else {
-				configuration.put(itemType, coloringLayer);
+				Entity matchingEntity = DecorationFromInputTag.getMatching(itemType, itemsRemovedFromConstruction);
+				if (matchingEntity == null) {
+					Logger.warn("Could not find matching entity with item type " + itemType.getItemTypeName() + " in " + this.getClass().getSimpleName());
+				} else {
+					ItemEntityAttributes sourceAttributes = (ItemEntityAttributes) matchingEntity.getPhysicalEntityComponent().getAttributes();
+					targetAttributes.setColor(targetColorLayer, sourceAttributes.getColor(sourceColorLayer));
+				}
 			}
 		}
 
-		FurnitureEntityAttributes targetAttributes = (FurnitureEntityAttributes) createdFurnitureEntity.getPhysicalEntityComponent().getAttributes();
-
-		for (Map.Entry<ItemType, ColoringLayer> configEntry : configuration.entrySet()) {
-			Entity matchingEntity = DecorationFromInputTag.getMatching(configEntry.getKey(), itemsRemovedFromConstruction);
-			if (matchingEntity == null) {
-				Logger.warn("Could not find matching entity with item type " + configEntry.getKey().getItemTypeName() + " in " + this.getClass().getSimpleName());
-			} else {
-				ItemEntityAttributes attributes = (ItemEntityAttributes) matchingEntity.getPhysicalEntityComponent().getAttributes();
-				targetAttributes.setColor(configEntry.getValue(), attributes.getPrimaryMaterial().getColor());
-			}
-		}
 	}
 }
