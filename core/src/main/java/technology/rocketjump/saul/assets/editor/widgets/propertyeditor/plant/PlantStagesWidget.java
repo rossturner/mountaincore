ce7703cc4354651f5e97549023a31056237017d0
MikeRocke <mikerocke1989@gmail.com>
2022-08-05 16:35:14 +0100

    Plant stages controls wip

--- core/src/main/java/technology/rocketjump/saul/assets/editor/widgets/propertyeditor/plant/PlantStagesWidget.java
+++ core/src/main/java/technology/rocketjump/saul/assets/editor/widgets/propertyeditor/plant/PlantStagesWidget.java
@@ -3,11 +3,16 @@ package technology.rocketjump.saul.assets.editor.widgets.propertyeditor.plant;
 import com.badlogic.gdx.ai.msg.MessageDispatcher;
 import com.kotcrab.vis.ui.widget.VisTable;
 import net.spookygames.gdx.nativefilechooser.NativeFileChooser;
+import technology.rocketjump.saul.assets.editor.widgets.ToStringDecorator;
+import technology.rocketjump.saul.assets.editor.widgets.propertyeditor.ColorsWidget;
+import technology.rocketjump.saul.assets.editor.widgets.propertyeditor.WidgetBuilder;
 import technology.rocketjump.saul.assets.entities.model.ColoringLayer;
+import technology.rocketjump.saul.entities.model.EntityType;
 import technology.rocketjump.saul.entities.model.physical.plant.PlantSpecies;
+import technology.rocketjump.saul.entities.model.physical.plant.PlantSpeciesGrowthStage;
 
 import java.nio.file.Path;
-import java.util.List;
+import java.util.*;
 
 public class PlantStagesWidget extends VisTable {
     private final PlantSpecies plantSpecies;
@@ -31,21 +36,81 @@ public class PlantStagesWidget extends VisTable {
         this.columnDefaults(0).left().uniformX();
         this.columnDefaults(1).expandX();
 
+        Collection<ToStringDecorator<Integer>> growthStageNumbers = new ArrayList<>();
+        growthStageNumbers.add(ToStringDecorator.none());
+        for (int i = 0; i < plantSpecies.getGrowthStages().size(); i++) {
+            growthStageNumbers.add(new ToStringDecorator<>(i, Object::toString));
+        }
+
+        for (int growthStageIndex = 0; growthStageIndex < plantSpecies.getGrowthStages().size(); growthStageIndex++) {
+            PlantSpeciesGrowthStage growthStage = plantSpecies.getGrowthStages().get(growthStageIndex);
+
+            if (growthStage != null) {
+                this.add(WidgetBuilder.label("Growth stage " + growthStageIndex)).padTop(20);
+                this.add(WidgetBuilder.button("x", x -> {
+                    plantSpecies.getGrowthStages().remove(growthStage);
+                    this.reload();
+                })).bottom().right();
+                this.row();
+                this.addSeparator().colspan(2);
+                this.row();
+
+                this.add(WidgetBuilder.label("Next Growth Stage"));
+                this.add(WidgetBuilder.select(new ToStringDecorator<>(growthStage.getNextGrowthStage(), Objects::toString), growthStageNumbers, null, selected -> {
+                    growthStage.setNextGrowthStage(selected.getObject());
+                })).fillX();
+                this.row();
+
+                this.add(WidgetBuilder.label("Seasons Until Complete"));
+                this.add(WidgetBuilder.doubleSpinner(growthStage.getSeasonsUntilComplete(), 0.0, Double.MAX_VALUE, growthStage::setSeasonsUntilComplete)).fillX();
+                this.row();
+
+                this.add(WidgetBuilder.label("Initial Plant Scale"));
+                this.add(WidgetBuilder.floatSpinner(growthStage.getInitialPlantScale(), 0.0f, Float.MAX_VALUE, growthStage::setInitialPlantScale)).fillX();
+                this.row();
+
+                this.add(WidgetBuilder.label("Complete Plant Scale"));
+                this.add(WidgetBuilder.floatSpinner(growthStage.getCompletionPlantScale(), 0.0f, Float.MAX_VALUE, growthStage::setCompletionPlantScale)).fillX();
+                this.row();
+
+                this.add(WidgetBuilder.label("Initial Fruit Scale"));
+                this.add(WidgetBuilder.floatSpinner(growthStage.getInitialFruitScale(), 0.0f, Float.MAX_VALUE, growthStage::setInitialFruitScale)).fillX();
+                this.row();
+
+                this.add(WidgetBuilder.label("Complete Fruit Scale"));
+                this.add(WidgetBuilder.floatSpinner(growthStage.getCompletionFruitScale(), 0.0f, Float.MAX_VALUE, growthStage::setCompletionPlantScale)).fillX();
+                this.row();
+
+                this.add(WidgetBuilder.label("Tile Height"));
+                this.add(WidgetBuilder.intSpinner(growthStage.getTileHeight(), 0, Integer.MAX_VALUE, growthStage::setTileHeight)).fillX();
+                this.row();
+
+                this.add(WidgetBuilder.label("Show Fruit"));
+                this.add(WidgetBuilder.toggle(growthStage.isShowFruit(), growthStage::setShowFruit)).fillX();
+                this.row();
+
+                this.add(WidgetBuilder.label("On Completion"));
+                this.add(WidgetBuilder.checkboxes(growthStage.getOnCompletion(), Arrays.asList(PlantSpeciesGrowthStage.PlantGrowthCompleteTag.values()), growthStage.getOnCompletion()::add, growthStage.getOnCompletion()::remove));
+                this.row();
+
+                this.add(WidgetBuilder.label("Colors"));
+                this.row();
+                this.add(new ColorsWidget(growthStage.getColors(), applicableColoringLayers,
+                        EntityType.PLANT, basePath, fileChooser, messageDispatcher)).colspan(2).pad(15);
+                this.row();
+
+
+                this.row();
+                this.addSeparator().colspan(2);
+                this.row();
+            }
+        }
+
 
 
         /*
-        private String name; // Mostly to help with debugging, not used by code (index in list of growth stages used instead)
-	private Integer nextGrowthStage = null; // Null representing no subsequent stage to go to
-	private double seasonsUntilComplete = 1;
 
-	private float initialPlantScale = 1;
-	private float completionPlantScale = 1;
-	private float initialFruitScale = 1;
-	private float completionFruitScale = 1;
-	private int tileHeight = 1;
-	private boolean showFruit = false;
 	private Map<ColoringLayer, SpeciesColor> colors = new EnumMap<>(ColoringLayer.class);
-	private List<PlantGrowthCompleteTag> onCompletion = new LinkedList<>();
 
 	private PlantSpeciesHarvestType harvestType = null;
 	private Integer harvestSwitchesToGrowthStage = null;
@@ -57,55 +122,7 @@ public class PlantStagesWidget extends VisTable {
 		LOGGING, FORAGING, FARMING
 
 	}
-
-	public enum PlantGrowthCompleteTag {
-
-		DISPERSE_SEEDS, DESTROY_PLANT
-
-	}
          */
 
-//        Collection<ToStringDecorator<Integer>> growthStageNumbers = new ArrayList<>();
-//        growthStageNumbers.add(ToStringDecorator.none());
-//        for (int i = 0; i < plantSpecies.getGrowthStages().size(); i++) {
-//            growthStageNumbers.add(new ToStringDecorator<>(i, Object::toString));
-//        }
-//
-//        for (Season season : Season.values()) {
-//            PlantSeasonSettings settings = plantSpecies.getSeasons().get(season);
-//            if (settings != null) {
-//                this.add(WidgetBuilder.label(season.name())).padTop(20);
-//                this.add(WidgetBuilder.button("x", x -> {
-//                    plantSpecies.getSeasons().remove(season);
-//                    this.reload();
-//                })).bottom().right();
-//                this.row();
-//                this.addSeparator().colspan(2);
-//                this.row();
-//
-//
-//                this.add(WidgetBuilder.label("Growth"));
-//                this.add(WidgetBuilder.toggle(settings.isGrowth(), settings::setGrowth)).fillX();
-//                this.row();
-//                this.add(WidgetBuilder.label("Sheds"));
-//                this.add(WidgetBuilder.toggle(settings.isShedsLeaves(), settings::setShedsLeaves)).fillX();
-//                this.row();
-//                this.add(WidgetBuilder.label("Switch to Growth Stage"));
-//                this.add(WidgetBuilder.select(new ToStringDecorator<>(settings.getSwitchToGrowthStage(), Objects::toString), growthStageNumbers, null, selected -> {
-//                    settings.setSwitchToGrowthStage(selected.getObject());
-//                })).fillX();
-//                this.row();
-//                this.add(WidgetBuilder.label("Colors"));
-//                this.row();
-//                this.add(new ColorsWidget(settings.getColors(), applicableColoringLayers,
-//                        EntityType.PLANT, basePath, fileChooser, messageDispatcher)).colspan(2).pad(15);
-//
-//                this.row();
-//                this.addSeparator().colspan(2);
-//                this.row();
-//            }
-//
-//            this.row();
-//        }
     }
 }
