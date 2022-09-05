c97a4ec60e69961db0cd9f59fc3192ad7115e632
MikeRocke <mikerocke1989@gmail.com>
2022-09-05 17:30:24 +0100

    Image processing shader that works on 3x3 matrix kernels Tiny bit concerned of the performance as these take 0.3ms a go round a fullscreen texture. potential for optimizations when i can actually profile the shader code for the slowdown (assuming its going to be the texture sampling)

--- core/src/main/resources/shaders/image_processing_fragment_shader.glsl
+++ core/src/main/resources/shaders/image_processing_fragment_shader.glsl
@@ -11,6 +11,7 @@ uniform mat3 u_kernelY;
 
 float step = 0.1;
 
+//TODO: maybe refactor to only have grayscale images to avoid thid calculation
 float intensity(in vec4 color){
     return sqrt((color.x*color.x)+(color.y*color.y)+(color.z*color.z));
 }
@@ -29,8 +30,15 @@ float convolution(float stepx, float stepy, vec2 center){
     float right = intensity(texture2D(u_texture, center + vec2(stepx, 0)));
     float bright = intensity(texture2D(u_texture, center + vec2(stepx, -stepy)));
 
-    float x = tleft + 2.0*left + bleft - tright - 2.0*right - bright;
-    float y = -tleft - 2.0*top - tright + bleft + 2.0 * bottom + bright;
+    float x = ( u_kernelX[0][0] * tleft +   u_kernelX[1][0] * top +      u_kernelX[2][0] * tright +
+                u_kernelX[0][1] * left +    u_kernelX[1][1] * middle +   u_kernelX[2][1] * right +
+                u_kernelX[0][2] * bleft +   u_kernelX[1][2] * bottom +   u_kernelX[2][2] * right);
+
+
+    float y = ( u_kernelY[0][0] * tleft +   u_kernelY[1][0] * top +      u_kernelY[2][0] * tright +
+                u_kernelY[0][1] * left +    u_kernelY[1][1] * middle +   u_kernelY[2][1] * right +
+                u_kernelY[0][2] * bleft +   u_kernelY[1][2] * bottom +   u_kernelY[2][2] * right);
+
     float newAlpha = sqrt((x*x) + (y*y));
     return newAlpha;
 }
@@ -43,5 +51,5 @@ void main() {
     );
 
     float newAlpha = convolution(step/u_viewportResolution.x, step/u_viewportResolution.y, correctedCoords);
-    gl_FragColor = vec4(newAlpha, newAlpha, newAlpha, newAlpha);
+    gl_FragColor = vec4(newAlpha, newAlpha, newAlpha, 1.0); //? probably wants a threshold for alpha, if above X then
 }
