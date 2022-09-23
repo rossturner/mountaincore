4feeaab6dfe21760592d0f4c88b39dd89aec8aa5
MikeRocke <mikerocke1989@gmail.com>
2022-09-23 11:59:21 +0100

    Mapping the function field to the BodyPartDefinition for use. Values likely to change

---
+++ core/src/main/java/technology/rocketjump/saul/entities/model/physical/creature/body/BodyPartFunction.java
@@ -0,0 +1,7 @@
+package technology.rocketjump.saul.entities.model.physical.creature.body;
+
+public enum BodyPartFunction {
+    GRASP,
+    FOOT
+    //todo: define specifics likes of main hand, off hand, steering/navigation whatever codebase uses
+}
