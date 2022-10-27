73bc6a381b3fc2ad5013ecb5507ebcec2ee14457
Ross Taylor-Turner <ross.turner001@googlemail.com>
2022-10-27 16:11:07 +0100

    #212 Updated EntityDrawable to handle furniture within bounds

--- core/src/main/java/technology/rocketjump/saul/ui/widgets/EntityDrawable.java
+++ core/src/main/java/technology/rocketjump/saul/ui/widgets/EntityDrawable.java
@@ -3,11 +3,14 @@ package technology.rocketjump.saul.ui.widgets;
 import com.badlogic.gdx.ai.msg.MessageDispatcher;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.utils.BaseDrawable;
 import technology.rocketjump.saul.assets.entities.item.model.ItemPlacement;
 import technology.rocketjump.saul.entities.model.Entity;
 import technology.rocketjump.saul.entities.model.physical.LocationComponent;
+import technology.rocketjump.saul.entities.model.physical.furniture.FurnitureEntityAttributes;
+import technology.rocketjump.saul.entities.model.physical.furniture.FurnitureLayout;
 import technology.rocketjump.saul.entities.model.physical.item.ItemEntityAttributes;
 import technology.rocketjump.saul.messaging.MessageType;
 import technology.rocketjump.saul.rendering.RenderMode;
@@ -45,8 +48,12 @@ public class EntityDrawable extends BaseDrawable {
         overrideLocationComponent.setFacing(DOWN.toVector2());
         entity.setLocationComponent(overrideLocationComponent);
 
-        float tempPixelsPerTile = PIXELS_PER_TILE;
-        PIXELS_PER_TILE = 1f;
+        float originalPixelsPerTile = PIXELS_PER_TILE;
+        PIXELS_PER_TILE = originalPixelsPerTile / width;
+        if (entity.getPhysicalEntityComponent().getAttributes() instanceof FurnitureEntityAttributes furnitureEntityAttributes) {
+            FurnitureLayout layout = furnitureEntityAttributes.getCurrentLayout();
+            adjustPositionForLayout(layout, x, y, width, height, overrideLocationComponent);
+        }
 
         revertToItemPlacement = null;
         if (showItemAsPlacedOnGround && entity.getPhysicalEntityComponent().getAttributes() instanceof ItemEntityAttributes attributes) {
@@ -65,9 +72,53 @@ public class EntityDrawable extends BaseDrawable {
             messageDispatcher.dispatchMessage(MessageType.ENTITY_ASSET_UPDATE_REQUIRED, entity);
         }
 
-        PIXELS_PER_TILE = tempPixelsPerTile;
+        PIXELS_PER_TILE = originalPixelsPerTile;
 
         entity.setLocationComponent(originalLocationComponent);
+    }
+
+    private void adjustPositionForLayout(FurnitureLayout layout, float x, float y, float width, float height, LocationComponent overrideLocationComponent) {
+        int minX = 0, maxX = 0, minY = 0, maxY = 0;
+        for (GridPoint2 extraTile : layout.getExtraTiles()) {
+            minX = Math.min(minX, extraTile.x);
+            minY = Math.min(minY, extraTile.y);
+            maxX = Math.max(maxX, extraTile.x);
+            maxY = Math.max(maxY, extraTile.y);
+        }
+
+        int tileWidth = 1 + maxX - minX;
+        int tileHeight = 1 + maxY - minY;
+        int maxTileAmount = Math.max(tileWidth, tileHeight);
+        // scale down a bit more
+        float amountAsFloat = 1.15f * (float)maxTileAmount;
+
+        PIXELS_PER_TILE = PIXELS_PER_TILE * amountAsFloat;
+
+        // Also need to offset the position based on the tile width and height
+        float tileSegmentWidth = width / maxTileAmount;
+        float tileSegmentHeight = height / maxTileAmount;
+
+        Vector2 positionFromCorner = new Vector2(tileSegmentWidth / 2, tileSegmentHeight / 2f);
+        while (minX < 0) {
+            positionFromCorner.x += tileSegmentWidth;
+            minX++;
+        }
+        while (minY < 0) {
+            positionFromCorner.y += tileSegmentHeight;
+            minY++;
+        }
+
+        // further adjustment to center non-square layouts
+        while (tileWidth > tileHeight) {
+            positionFromCorner.y += tileSegmentHeight / 2;
+            tileHeight++;
+        }
+        while (tileHeight > tileWidth) {
+            positionFromCorner.x += tileSegmentWidth / 2;
+            tileWidth++;
+        }
+
+        overrideLocationComponent.setWorldPosition(positionFromCorner.add(x, y), false);
     }
 
     public Color getOverrideColor() {
