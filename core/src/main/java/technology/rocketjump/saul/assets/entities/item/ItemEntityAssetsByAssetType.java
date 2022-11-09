4d4e822be5ee1b4e562df6a817cef87a36c69264
Ross Taylor-Turner <ross.turner001@googlemail.com>
2022-11-09 15:24:59 +0000

    Removing unnecessary nodes in ItemEntityAsset maps

--- core/src/main/java/technology/rocketjump/saul/assets/entities/item/ItemEntityAssetsByAssetType.java
+++ core/src/main/java/technology/rocketjump/saul/assets/entities/item/ItemEntityAssetsByAssetType.java
@@ -1,10 +1,10 @@
 package technology.rocketjump.saul.assets.entities.item;
 
-import technology.rocketjump.saul.assets.entities.EntityAssetTypeDictionary;
+import org.pmw.tinylog.Logger;
 import technology.rocketjump.saul.assets.entities.item.model.ItemEntityAsset;
 import technology.rocketjump.saul.assets.entities.model.EntityAssetType;
-import technology.rocketjump.saul.entities.model.EntityType;
 import technology.rocketjump.saul.entities.model.physical.item.ItemEntityAttributes;
+import technology.rocketjump.saul.entities.model.physical.item.ItemType;
 import technology.rocketjump.saul.entities.model.physical.item.ItemTypeDictionary;
 
 import java.util.HashMap;
@@ -18,21 +18,19 @@ public class ItemEntityAssetsByAssetType {
 	private final ItemTypeDictionary itemTypeDictionary;
 	private Map<EntityAssetType, ItemEntityAssetsByItemType> typeMap = new HashMap<>();
 
-	public ItemEntityAssetsByAssetType(EntityAssetTypeDictionary typeDictionary, ItemTypeDictionary itemTypeDictionary) {
+	public ItemEntityAssetsByAssetType(ItemTypeDictionary itemTypeDictionary) {
 		this.itemTypeDictionary = itemTypeDictionary;
-		for (EntityAssetType assetType : typeDictionary.getByEntityType(EntityType.ITEM)) {
-			typeMap.put(assetType, new ItemEntityAssetsByItemType(itemTypeDictionary));
-		}
-		typeMap.put(NULL_ENTITY_ASSET_TYPE, new ItemEntityAssetsByItemType(itemTypeDictionary));
+		typeMap.put(NULL_ENTITY_ASSET_TYPE, new ItemEntityAssetsByItemType());
 	}
 
 	public void add(ItemEntityAsset asset) {
-		// Assuming all entities have a type specified
-		if (!typeMap.containsKey(asset.getType())) {
-			throw new RuntimeException("Unrecognised asset type " + asset.getType() + " for " + asset.getUniqueName());
+		ItemType itemType = itemTypeDictionary.getByName(asset.getItemTypeName());
+		if (itemType == null) {
+			Logger.error(asset.getUniqueName() + " asset does not have a valid item type specified");
+			return;
 		}
-
-		typeMap.get(asset.getType()).add(itemTypeDictionary.getByName(asset.getItemTypeName()), asset);
+		typeMap.computeIfAbsent(asset.getType(), a -> new ItemEntityAssetsByItemType())
+				.add(itemType, asset);
 		typeMap.get(NULL_ENTITY_ASSET_TYPE).add(itemTypeDictionary.getByName(asset.getItemTypeName()), asset);
 	}
 
@@ -40,15 +38,13 @@ public class ItemEntityAssetsByAssetType {
 		if (entityAssetType == null) {
 			entityAssetType = NULL_ENTITY_ASSET_TYPE;
 		}
-		return typeMap.get(entityAssetType).get(attributes);
+		ItemEntityAssetsByItemType childMap = typeMap.get(entityAssetType);
+		return childMap != null ? childMap.get(attributes) : null;
 	}
 
 	public List<ItemEntityAsset> getAll(EntityAssetType entityAssetType, ItemEntityAttributes attributes) {
-		return typeMap.get(entityAssetType).getAll(attributes);
-	}
-
-	public ItemEntityAssetsByItemType getItemTypeMapByAssetType(EntityAssetType assetType) {
-		return typeMap.get(assetType);
+		ItemEntityAssetsByItemType childMap = typeMap.get(entityAssetType);
+		return childMap != null ? childMap.getAll(attributes) : List.of();
 	}
 
 }
