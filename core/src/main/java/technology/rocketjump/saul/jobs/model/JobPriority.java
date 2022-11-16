626ef424fa6b9724f514204b54d6afe115b7df75
Ross Taylor-Turner <ross.turner001@googlemail.com>
2022-11-16 14:31:36 +0000

    #213 Priority menu buttons

--- core/src/main/java/technology/rocketjump/saul/jobs/model/JobPriority.java
+++ core/src/main/java/technology/rocketjump/saul/jobs/model/JobPriority.java
@@ -6,12 +6,12 @@ import technology.rocketjump.saul.rendering.utils.HexColors;
 public enum JobPriority {
 
 	// These are expected to be in order of highest to lowest priority
-	HIGHEST("#265aaa", "fast-forward-button-up", "PRIORITY.HIGHEST", "btn_crafting_priority_5"),
-	HIGHER("#269caa", "play-button-up", "PRIORITY.HIGHER", "btn_crafting_priority_4"),
-	NORMAL("#27aa5e", "play-button", "PRIORITY.NORMAL", "btn_crafting_priority_3"),
-	LOWER("#bab524", "play-button-down", "PRIORITY.LOWER", "btn_crafting_priority_2"),
-	LOWEST("#aa8026", "fast-forward-button-down", "PRIORITY.LOWEST", "btn_crafting_priority_1"),
-	DISABLED("#D4534C", "cancel", "PRIORITY.DISABLED", "btn_crafting_priority_0");
+	HIGHEST("#265aaa", "fast-forward-button-up", "PRIORITY.HIGHEST", "btn_crafting_priority_5", "btn_priority_5"),
+	HIGHER("#269caa", "play-button-up", "PRIORITY.HIGHER", "btn_crafting_priority_4", "btn_priority_4"),
+	NORMAL("#27aa5e", "play-button", "PRIORITY.NORMAL", "btn_crafting_priority_3", "btn_priority_3"),
+	LOWER("#bab524", "play-button-down", "PRIORITY.LOWER", "btn_crafting_priority_2", "btn_priority_2"),
+	LOWEST("#aa8026", "fast-forward-button-down", "PRIORITY.LOWEST", "btn_crafting_priority_1", "btn_priority_1"),
+	DISABLED("#D4534C", "cancel", "PRIORITY.DISABLED", "btn_crafting_priority_0", "btn_priority_0");
 
 	@Deprecated
 	public final Color color;
@@ -20,15 +20,17 @@ public enum JobPriority {
 	@Deprecated
 	public final String iconName;
 	public final String i18nKey;
-	public final String drawableName;
+	public final String craftingDrawableName;
+	public final String buttonDrawableName;
 
-	JobPriority(String hexColor, String iconName, String i18nKey, String drawableName) {
+	JobPriority(String hexColor, String iconName, String i18nKey, String craftingDrawableName, String buttonDrawableName) {
 		this.color = HexColors.get(hexColor);
+		this.buttonDrawableName = buttonDrawableName;
 		this.semiTransparentColor = this.color.cpy();
 		this.semiTransparentColor.a = 0.8f;
 		this.iconName = iconName;
 		this.i18nKey = i18nKey;
-		this.drawableName = drawableName;
+		this.craftingDrawableName = craftingDrawableName;
 	}
 
 }
--- core/src/main/java/technology/rocketjump/saul/ui/views/PrioritiesGuiView.java
+++ core/src/main/java/technology/rocketjump/saul/ui/views/PrioritiesGuiView.java
@@ -2,47 +2,69 @@ package technology.rocketjump.saul.ui.views;
 
 import com.badlogic.gdx.ai.msg.MessageDispatcher;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import technology.rocketjump.saul.jobs.model.JobPriority;
 import technology.rocketjump.saul.messaging.MessageType;
-import technology.rocketjump.saul.rendering.utils.HexColors;
 import technology.rocketjump.saul.ui.GameInteractionMode;
 import technology.rocketjump.saul.ui.GameViewMode;
-import technology.rocketjump.saul.ui.actions.SwitchGuiViewAction;
-import technology.rocketjump.saul.ui.widgets.ButtonStyle;
-import technology.rocketjump.saul.ui.widgets.IconButton;
-import technology.rocketjump.saul.ui.widgets.IconButtonFactory;
+import technology.rocketjump.saul.ui.cursor.GameCursor;
+import technology.rocketjump.saul.ui.eventlistener.ChangeCursorOnHover;
+import technology.rocketjump.saul.ui.eventlistener.TooltipFactory;
+import technology.rocketjump.saul.ui.eventlistener.TooltipLocationHint;
+import technology.rocketjump.saul.ui.i18n.DisplaysText;
+import technology.rocketjump.saul.ui.skins.GuiSkinRepository;
 
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-import static technology.rocketjump.saul.ui.views.GuiViewName.DEFAULT_MENU;
-
 @Singleton
-public class PrioritiesGuiView implements GuiView {
+public class PrioritiesGuiView implements GuiView, DisplaysText {
 
 	private final MessageDispatcher messageDispatcher;
+	private final Skin skin;
+	private final TooltipFactory tooltipFactory;
 	private List<Actor> buttons = new LinkedList<>();
+	private Button backButton;
 
 	@Inject
-	public PrioritiesGuiView(IconButtonFactory iconButtonFactory, MessageDispatcher messageDispatcher) {
+	public PrioritiesGuiView(GuiSkinRepository skinRepository, MessageDispatcher messageDispatcher,
+							 TooltipFactory tooltipFactory) {
 		this.messageDispatcher = messageDispatcher;
+		this.skin = skinRepository.getMainGameSkin();
+		this.tooltipFactory = tooltipFactory;
+	}
 
-
-		IconButton back = iconButtonFactory.create("GUI.BACK_LABEL", "arrow-left", HexColors.get("#D9D9D9"), ButtonStyle.DEFAULT);
-		back.setAction(new SwitchGuiViewAction(DEFAULT_MENU, messageDispatcher));
-		buttons.add(back);
+	@Override
+	public void rebuildUI() {
+		backButton = new Button(skin.getDrawable("btn_back"));
+		backButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				messageDispatcher.dispatchMessage(MessageType.GUI_SWITCH_VIEW, getParentViewName());
+			}
+		});
+		backButton.addListener(new ChangeCursorOnHover(backButton, GameCursor.SELECT, messageDispatcher));
+		tooltipFactory.simpleTooltip(backButton, "GUI.BACK_LABEL", TooltipLocationHint.ABOVE);
 
 		List<JobPriority> prioritiesLowToHigh = Arrays.asList(JobPriority.LOWEST, JobPriority.LOWER, JobPriority.NORMAL, JobPriority.HIGHER, JobPriority.HIGHEST);
 		for (JobPriority jobPriority : prioritiesLowToHigh) {
-			IconButton button = iconButtonFactory.create(jobPriority.i18nKey, jobPriority.iconName, jobPriority.color, ButtonStyle.DEFAULT);
-			button.setAction(() -> {
-				messageDispatcher.dispatchMessage(MessageType.REPLACE_JOB_PRIORITY, jobPriority);
-				messageDispatcher.dispatchMessage(MessageType.GUI_SWITCH_INTERACTION_MODE, GameInteractionMode.SET_JOB_PRIORITY);
+			Button button = new Button(skin.getDrawable(jobPriority.buttonDrawableName));
+			button.addListener(new ClickListener() {
+				@Override
+				public void clicked(InputEvent event, float x, float y) {
+					messageDispatcher.dispatchMessage(MessageType.REPLACE_JOB_PRIORITY, jobPriority);
+					messageDispatcher.dispatchMessage(MessageType.GUI_SWITCH_INTERACTION_MODE, GameInteractionMode.SET_JOB_PRIORITY);
+				}
 			});
+			button.addListener(new ChangeCursorOnHover(button, GameCursor.SELECT, messageDispatcher));
+			tooltipFactory.simpleTooltip(button, jobPriority.i18nKey, TooltipLocationHint.ABOVE);
 			buttons.add(button);
 		}
 	}
@@ -59,10 +81,11 @@ public class PrioritiesGuiView implements GuiView {
 
 	@Override
 	public void populate(Table containerTable) {
+		containerTable.clear();
+		containerTable.add(backButton).center().padLeft(30f).padRight(23f).padBottom(10f);
 		for (Actor button : buttons) {
-			containerTable.add(button).pad(5);
+			containerTable.add(button).padLeft(23f).padBottom(17f);
 		}
-
 	}
 
 	@Override
