5f86bdf35f587efc5d36c33f018875829c016d1f
Ross Taylor-Turner <ross.turner001@googlemail.com>
2022-11-21 16:57:27 +0000

    Game view mode buttons

--- core/src/main/java/technology/rocketjump/saul/ui/GameViewMode.java
+++ core/src/main/java/technology/rocketjump/saul/ui/GameViewMode.java
@@ -4,8 +4,16 @@ public enum GameViewMode {
 
 	DEFAULT,
 	JOB_PRIORITY,
+	ROOFING_INFO,
 	PIPING,
-	MECHANISMS,
-	ROOFING_INFO;
+	MECHANISMS;
+
+	public String getButtonStyleName() {
+		return "view_mode_" + name().toLowerCase();
+	}
+
+	public String getI18nKey() {
+		return "GUI.VIEW_MODE." + name();
+	}
 
 }
