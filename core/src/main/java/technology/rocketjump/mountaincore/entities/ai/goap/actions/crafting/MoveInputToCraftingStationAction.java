4fa6d0f5816fe3069aa314702fa73fbb553cdbff
MikeRocke <mikerocke1989@gmail.com>
2023-04-11 12:25:55 +0100

    Destroying sub goals if the wrapping goal is interrupted

--- core/src/main/java/technology/rocketjump/mountaincore/entities/ai/goap/actions/crafting/MoveInputToCraftingStationAction.java
+++ core/src/main/java/technology/rocketjump/mountaincore/entities/ai/goap/actions/crafting/MoveInputToCraftingStationAction.java
@@ -33,6 +33,15 @@ public class MoveInputToCraftingStationAction extends Action implements Initiali
 	}
 
 	@Override
+	public void actionInterrupted(GameContext gameContext) {
+		super.actionInterrupted(gameContext);
+		if (subGoal != null) {
+			subGoal.setInterrupted(true);
+			subGoal.destroy(null, parent.messageDispatcher, gameContext);
+		}
+	}
+
+	@Override
 	public void update(float deltaTime, GameContext gameContext) throws SwitchGoalException {
 		if (subGoal == null) {
 			initialiseHaulingOfInput(gameContext);
