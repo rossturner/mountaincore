46aedee2577ad6bf8718e980e070b790de38b02e
MikeRocke <mikerocke1989@gmail.com>
2023-04-25 17:09:06 +0100

    tweaked the empty location

--- core/src/main/java/technology/rocketjump/mountaincore/entities/ai/goap/actions/location/GoToRandomEmptyLocationAction.java
+++ core/src/main/java/technology/rocketjump/mountaincore/entities/ai/goap/actions/location/GoToRandomEmptyLocationAction.java
@@ -1,53 +1,35 @@
 package technology.rocketjump.mountaincore.entities.ai.goap.actions.location;
 
-import com.alibaba.fastjson.JSONObject;
 import com.badlogic.gdx.math.Vector2;
 import technology.rocketjump.mountaincore.entities.ai.goap.AssignedGoal;
+import technology.rocketjump.mountaincore.entities.ai.goap.actions.IdleAction;
 import technology.rocketjump.mountaincore.entities.model.physical.LocationComponent;
 import technology.rocketjump.mountaincore.gamecontext.GameContext;
 import technology.rocketjump.mountaincore.mapping.model.TiledMap;
 import technology.rocketjump.mountaincore.mapping.tile.MapTile;
-import technology.rocketjump.mountaincore.persistence.SavedGameDependentDictionaries;
-import technology.rocketjump.mountaincore.persistence.model.InvalidSaveException;
-import technology.rocketjump.mountaincore.persistence.model.SavedGameStateHolder;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashSet;
 import java.util.Set;
 
-import static technology.rocketjump.mountaincore.entities.ai.goap.actions.Action.CompletionType.FAILURE;
-import static technology.rocketjump.mountaincore.entities.ai.goap.actions.Action.CompletionType.SUCCESS;
-
 public class GoToRandomEmptyLocationAction extends GoToLocationAction {
 
-	private static final float MAX_ELAPSED_TIME = 10f;
-	private static final int LIMIT = 10;
-
-	private float elapsedTime;
+	private static final int MAX_DISTANCE = 10;
+	private static final int MIN_DISTANCE = 3; //for aesthetics
 
 	public GoToRandomEmptyLocationAction(AssignedGoal parent) {
 		super(parent);
 	}
 
 	@Override
-	public void update(float deltaTime, GameContext gameContext) {
-		if (overrideLocation == null) {
-			Vector2 target = findEmptyLocation(gameContext);
-			if (target != null) {
-				overrideLocation = target;
-			} else {
-				completionType = FAILURE;
-				return;
-			}
+	protected Vector2 selectDestination(GameContext gameContext) {
+		Vector2 location = findEmptyLocation(gameContext);
+		if (location == null) {
+			return IdleAction.pickRandomLocation(gameContext, parent.parentEntity);
+		} else {
+			return location;
 		}
-
-		elapsedTime += deltaTime;
-		if (elapsedTime > MAX_ELAPSED_TIME) {
-			completionType = SUCCESS;
-		}
-
-		super.update(deltaTime, gameContext);
 	}
 
 	private Vector2 findEmptyLocation(GameContext gameContext) {
@@ -63,14 +45,15 @@ public class GoToRandomEmptyLocationAction extends GoToLocationAction {
 		while (!frontier.isEmpty()) {
 			MapTile currentTile = frontier.pop();
 			explored.add(currentTile);
-			if (currentTile.isEmpty()) {
+			if (currentTile.isEmpty() &&
+				distance(startingTile, currentTile) >= MIN_DISTANCE) {
 				return currentTile.getWorldPositionOfCenter();
 			}
 
 			for (MapTile neighbour : areaMap.getNeighbours(currentTile.getTileX(), currentTile.getTileY()).values()) {
 				if (!explored.contains(neighbour) &&
 						neighbour.getRegionId() == currentTile.getRegionId() &&
-						distance(startingTile, currentTile) < LIMIT) {
+						distance(startingTile, currentTile) < MAX_DISTANCE) {
 					frontier.add(neighbour);
 				}
 			}
@@ -83,17 +66,4 @@ public class GoToRandomEmptyLocationAction extends GoToLocationAction {
 		return Math.abs(a.getTileX() - b.getTileX()) + Math.abs(a.getTileY() - b.getTileY());
 	}
 
-	@Override
-	public void writeTo(JSONObject asJson, SavedGameStateHolder savedGameStateHolder) {
-		super.writeTo(asJson, savedGameStateHolder);
-
-		asJson.put("elapsed", elapsedTime);
-	}
-
-	@Override
-	public void readFrom(JSONObject asJson, SavedGameStateHolder savedGameStateHolder, SavedGameDependentDictionaries relatedStores) throws InvalidSaveException {
-		super.readFrom(asJson, savedGameStateHolder, relatedStores);
-
-		this.elapsedTime = asJson.getFloatValue("elapsed");
-	}
 }
