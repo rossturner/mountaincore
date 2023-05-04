ec9991228b58b9571c8b4ae0e1600baa04dbfb9c
MikeRocke <mikerocke1989@gmail.com>
2023-05-04 15:25:45 +0100

    Fixing more loops in crafting to terminate if no progress made after 3 attempts

--- core/src/main/java/technology/rocketjump/mountaincore/entities/ai/goap/actions/crafting/MoveLiquidInputToCraftingStationAction.java
+++ core/src/main/java/technology/rocketjump/mountaincore/entities/ai/goap/actions/crafting/MoveLiquidInputToCraftingStationAction.java
@@ -19,11 +19,15 @@ import technology.rocketjump.mountaincore.persistence.model.InvalidSaveException
 import technology.rocketjump.mountaincore.persistence.model.SavedGameStateHolder;
 import technology.rocketjump.mountaincore.rooms.HaulingAllocation;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MoveLiquidInputToCraftingStationAction extends Action implements InitialisableAction {
 
+	public static final int MAX_ATTEMPTS_PER_LIQUID_ALLOCATION = 3;
 	private AssignedGoal subGoal;
+	private final Map<Integer, Integer> transferAttempts = new HashMap<>();
 
 	public MoveLiquidInputToCraftingStationAction(AssignedGoal parent) {
 		super(parent);
@@ -48,14 +52,14 @@ public class MoveLiquidInputToCraftingStationAction extends Action implements In
 
 	@Override
 	public void update(float deltaTime, GameContext gameContext) throws SwitchGoalException {
-		if (subGoal == null) {
-			initialiseLiquidTransferInput(gameContext);
-		} else {
+		if (subGoal != null) {
 			if (subGoal.isComplete()) {
-				completionType = CompletionType.SUCCESS;
+				initialiseLiquidTransferInput(gameContext);
 			} else {
 				subGoal.update(deltaTime, gameContext);
 			}
+		} else {
+			initialiseLiquidTransferInput(gameContext);
 		}
 	}
 
@@ -64,39 +68,52 @@ public class MoveLiquidInputToCraftingStationAction extends Action implements In
 		if (craftingStation != null && craftingStation.getBehaviourComponent() instanceof CraftingStationBehaviour craftingStationBehaviour
 				&& craftingStationBehaviour.getCurrentCraftingAssignment() != null) {
 
-			ItemType itemType = craftingStationBehaviour.getRelatedItemTypes().get(0);
-			if (itemType == null) {
-				Logger.error("Expecting related item type on crafting station {}", craftingStation);
-				completionType = CompletionType.FAILURE;
-				return;
-			}
-
 			List<LiquidAllocation> liquidAllocations = craftingStationBehaviour.getCurrentCraftingAssignment().getInputLiquidAllocations();
-			LiquidAllocation liquidAllocation = liquidAllocations.remove(0);
-
-			subGoal = new AssignedGoal(SpecialGoal.TRANSFER_LIQUID_FOR_CRAFTING.getInstance(), parent.parentEntity, parent.messageDispatcher, gameContext);
-			subGoal.setAssignedJob(parent.getAssignedJob());
-
-			// need a hauling allocation of an item to carry the liquid in
-			parent.messageDispatcher.dispatchMessage(MessageType.REQUEST_HAULING_ALLOCATION, new RequestHaulingAllocationMessage(
-					parent.parentEntity, parent.parentEntity.getLocationComponent().getWorldOrParentPosition(), itemType,
-					null, // any material
-					true, null, null, (allocation) -> {
-				if (allocation != null) {
-					allocation.setTargetPositionType(HaulingAllocation.AllocationPositionType.ZONE);
-					allocation.setTargetId(liquidAllocation.getTargetContainerId());
-					allocation.setTargetPosition(liquidAllocation.getTargetZoneTile().getAccessLocation());
-
-					subGoal.setAssignedHaulingAllocation(allocation);
-				}
-			}));
-
-			if (subGoal.getAssignedHaulingAllocation() == null) {
-				completionType = CompletionType.FAILURE;
-				liquidAllocations.add(liquidAllocation);
+			if (liquidAllocations.isEmpty()) {
+				completionType = CompletionType.SUCCESS;
 			} else {
-				subGoal.setLiquidAllocation(liquidAllocation);
-				subGoal.setParentGoal(this.parent);
+
+				if (craftingStationBehaviour.getRelatedItemTypes().isEmpty() || craftingStationBehaviour.getRelatedItemTypes().get(0) == null) {
+					completionType = CompletionType.FAILURE;
+					Logger.error("Expecting related item type on crafting station {}", craftingStation);
+					return;
+				}
+				ItemType itemType = craftingStationBehaviour.getRelatedItemTypes().get(0);
+
+				LiquidAllocation liquidAllocation = liquidAllocations.remove(0);
+				Integer attempt = transferAttempts.getOrDefault(liquidAllocations.size(), 0);
+				if (attempt > MAX_ATTEMPTS_PER_LIQUID_ALLOCATION) {
+					liquidAllocations.add(liquidAllocation);
+					completionType = CompletionType.FAILURE;
+					return;
+				} else {
+					transferAttempts.put(liquidAllocations.size(), attempt + 1);
+				}
+
+				subGoal = new AssignedGoal(SpecialGoal.TRANSFER_LIQUID_FOR_CRAFTING.getInstance(), parent.parentEntity, parent.messageDispatcher, gameContext);
+				subGoal.setAssignedJob(parent.getAssignedJob());
+
+				// need a hauling allocation of an item to carry the liquid in
+				parent.messageDispatcher.dispatchMessage(MessageType.REQUEST_HAULING_ALLOCATION, new RequestHaulingAllocationMessage(
+						parent.parentEntity, parent.parentEntity.getLocationComponent().getWorldOrParentPosition(), itemType,
+						null, // any material
+						true, null, null, (allocation) -> {
+					if (allocation != null) {
+						allocation.setTargetPositionType(HaulingAllocation.AllocationPositionType.ZONE);
+						allocation.setTargetId(liquidAllocation.getTargetContainerId());
+						allocation.setTargetPosition(liquidAllocation.getTargetZoneTile().getAccessLocation());
+
+						subGoal.setAssignedHaulingAllocation(allocation);
+					}
+				}));
+
+				if (subGoal.getAssignedHaulingAllocation() == null) {
+					completionType = CompletionType.FAILURE;
+					liquidAllocations.add(liquidAllocation);
+				} else {
+					subGoal.setLiquidAllocation(liquidAllocation);
+					subGoal.setParentGoal(this.parent);
+				}
 			}
 		} else {
 			completionType = CompletionType.FAILURE;
