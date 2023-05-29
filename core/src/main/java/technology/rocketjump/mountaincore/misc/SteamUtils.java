dc2aabcebb0b9497658a4a5a1f5eb41bacb204e7
Ross Taylor-Turner <ross.turner001@googlemail.com>
2023-05-29 22:04:48 +0100

    Working around Steam on Mac

--- core/src/main/java/technology/rocketjump/mountaincore/misc/SteamUtils.java
+++ core/src/main/java/technology/rocketjump/mountaincore/misc/SteamUtils.java
@@ -1,19 +1,21 @@
 package technology.rocketjump.mountaincore.misc;
 
 import com.codedisaster.steamworks.SteamAPI;
+import org.apache.commons.lang3.SystemUtils;
 
 public class SteamUtils {
 
 	public static com.codedisaster.steamworks.SteamUtils utils;
 	static {
 		try {
-			utils = new com.codedisaster.steamworks.SteamUtils(() -> {
-			});
+			if (!SystemUtils.IS_OS_MAC) {
+				utils = new com.codedisaster.steamworks.SteamUtils(() -> {});
+			}
 		} catch (Throwable ignored) {}
 	}
 
 	public static boolean isRunningOnSteamDeck() {
-		return SteamAPI.isSteamRunning() && utils != null && utils.isSteamRunningOnSteamDeck();
+		return utils != null && SteamAPI.isSteamRunning() && utils.isSteamRunningOnSteamDeck();
 	}
 
 }
